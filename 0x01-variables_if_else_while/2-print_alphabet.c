b66b24606fda9b41b215d3d10cc2b1804de86d34
LuntuZAR <luntukazinkungwini@gmail.com>
2023-02-17 16:05:14 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints in lowercase,
+ * followed by new line
+ * Return: 0 (Accomplished)
+ */
+int main(void)
+{
+	char ch;
+
+	
+
+	for 
+	(ch = 'a'; i ch < = 'z'; ch++);
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
