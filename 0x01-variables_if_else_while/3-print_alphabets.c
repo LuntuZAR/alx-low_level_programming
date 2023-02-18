2325ad1e04388fcc11cc14e09e9a988676cd8394
LuntuZAR <luntukazinkungwini@gmail.com>
2023-02-17 17:10:03 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 'a'; ch < = 'z' ch++)
+putchar(ch);
+for (ch = 'A' < = 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
