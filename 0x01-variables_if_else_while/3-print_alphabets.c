7d989e04a314bc25251b46cdd1ad7d55fcd8abd0
LuntuZAR <luntukazinkungwini@gmail.com>
2023-02-17 17:25:33 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 int ch;
 
-for (ch = 'a'; ch <= 'z'; ch++);
+for (ch = 'a'; ch <= 'z'; ch++;)
 putchar(ch);
-for (ch = 'A' <= 'Z'; ch++);
+for (ch = 'A' <= 'Z'; ch++;)
 putchar(ch);
 putchar('\n');
 return (0);
