b02b6a35fa31e7594806d4746572137fa0a1971f
LuntuZAR <luntukazinkungwini@gmail.com>
2023-02-22 17:11:55 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#include"main_h"
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +12,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
 
